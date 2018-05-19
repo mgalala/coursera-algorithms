913c32056f5b306dce590dac3ef230e6245c7c89
Muhammad Allam <mallam.ext@gmail.com>
2018-05-19 14:22:38 +0200

    quick-find impl

---
+++ algorithms/src/main/java/com/mgalala/algorithms/QuickFind.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.mgalala.algorithms;
+
+/**
+ * @author mgalala
+ *
+ */
+public class QuickFind {
+
+	private int[] id;
+
+	public QuickFind(int total) {
+		id = new int[total];
+		for (int i = 0; i < total; i++) {
+			id[i] = i;
+		}
+	}
+
+	public boolean connected(int p, int q) {
+		return id[p] == id[q];
+	}
+
+	public void union(int p, int q) {
+		int pId = id[p];
+		int qId = id[q];
+		for (int i = 0; i < id.length; i++) {
+			if (id[i] == pId) {
+				id[i] = qId;
+			}
+		}
+	}
+
+}
---
+++ algorithms/src/test/java/com/mgalala/algoirthms/QuickFindTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.mgalala.algoirthms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mgalala.algorithms.QuickFind;
+
+/**
+ * @author mgalala
+ *
+ */
+public class QuickFindTest {
+	private QuickFind quickFind;
+
+	@Test
+	public void testUnionFind() {
+		quickFind = new QuickFind(10);
+		quickFind.union(0, 5);
+		quickFind.union(0, 6);
+		Assert.assertTrue(quickFind.connected(0, 5));
+		Assert.assertTrue(quickFind.connected(0, 6));
+		Assert.assertFalse(quickFind.connected(0, 7));
+	}
+}
