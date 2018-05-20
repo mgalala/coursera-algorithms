3d66514440468d38e5eb7c78fbfce1143debf57b
Muhammad Allam <mallam.ext@gmail.com>
2018-05-20 14:23:16 +0200

    answer to question &

---
+++ algorithms/src/main/java/com/mgalala/algorithms/excersie/Question1.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.mgalala.algorithms.excersie;
+
+/**
+ * @author mgalala Social network connectivity. Given a social network
+ *         containing nn members and a log file containing mm timestamps at
+ *         which times pairs of members formed friendships, design an algorithm
+ *         to determine the earliest time at which all members are connected
+ *         (i.e., every member is a friend of a friend of a friend ... of a
+ *         friend). Assume that the log file is sorted by timestamp and that
+ *         friendship is an equivalence relation. The running time of your
+ *         algorithm should be m \log nmlogn or better and use extra space
+ *         proportional to nn.
+ * 
+ *         Note: these interview questions are ungraded and purely for your own
+ *         enrichment. To get a hint, submit a solution.
+ */
+public class Question1 {
+	private int[] id;
+	private int[] size;
+
+	private int totalConnectedComponents;
+
+	public Question1(int total) {
+		id = new int[total];
+		size = new int[total];
+		totalConnectedComponents = 1;// we begin with 1 because once 2 items are
+										// connected then, this will be
+										// incremented.
+		for (int i = 0; i < total; i++) {
+			id[i] = i;
+			size[i] = 1;
+		}
+	}
+
+	public boolean connected(int p, int q) {
+		return rootOf(p) == rootOf(q);
+	}
+
+	public void union(int p, int q) {
+		int rootOfP = rootOf(p);
+		int rootOfQ = rootOf(q);
+		if (rootOfP == rootOfQ) {
+			return;
+		}
+
+		if (size[rootOfP] < size[rootOfQ]) {
+			// p is the lower tree, then move p to be under q and increase the
+			// size of q tree.
+			id[rootOfP] = rootOfQ;
+			// size[rootOfQ] = size[rootOfQ] + size[rootOfP];
+			size[rootOfQ] += size[rootOfP];
+		} else {
+			// p is the bigger tree, then move q to be under p and increase the
+			// size of p tree.
+			id[rootOfQ] = rootOfP;
+			// size[rootOfP] = size[rootOfQ] + size[rootOfP];
+			size[rootOfP] += size[rootOfQ];
+		}
+		++totalConnectedComponents;
+	}
+
+	private int rootOf(int i) {
+		while (i != id[i]) {
+			id[i] = id[id[i]];
+			i = id[i];
+		}
+		return i;
+	}
+
+	public int[] getId() {
+		return id;
+	}
+
+	public int[] getSize() {
+		return size;
+	}
+
+	public int getCount() {
+		return totalConnectedComponents;
+	}
+
+}
---
+++ algorithms/src/test/java/com/mgalala/algoirthms/exercise/Question1Test.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.mgalala.algoirthms.exercise;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mgalala.algorithms.excersie.Question1;
+
+/**
+ * @author mgalala
+ *
+ */
+public class Question1Test {
+
+	private Question1 weightedQuickUnion;
+
+	@Test
+	public void testWeightedUnionFind() {
+		weightedQuickUnion = new Question1(10);
+		weightedQuickUnion.union(0, 5);
+		weightedQuickUnion.union(0, 6);
+		weightedQuickUnion.union(7, 2);
+		weightedQuickUnion.union(2, 0);
+		weightedQuickUnion.union(9, 0);
+		weightedQuickUnion.union(7, 3);
+
+		Assert.assertEquals(7, weightedQuickUnion.getCount());
+	}
+}
